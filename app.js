06201bd80f1a85eea51d4184fef7483cfba9cd99
Ashish Bora <138502014+ashishborakv@users.noreply.github.com>
2023-07-05 10:46:02 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log('Hello, neoGrammers! 🙏🏻');
+
+function isOne(num) {
+    return num == 1;
+}
+
+function isBigger(a, b) {
+    return a > b;
+}
+console.log('Is 2 bigger than 3?', isBigger(2, 3));
+
+const moreThan22 = (num) => num > 22;
+
+function giveMeAnObject(a) {
+    return {
+        value: a,
+    };
+}
